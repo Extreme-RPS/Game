06b9c7e2d404f9a41073c0d7c7934b43a38d4d4c
ZStAmant <teromafia@yahoo.com>
2014-09-15 13:23:57 -0400

    TESTAPP
    UPLOAD

---
+++ TEST/TestApp/src/GIT/ZStAmant/testapp/MainActivity.java
@@ -0,0 +1,15 @@
+package GIT.ZStAmant.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
